3694990485c9ed6b17f0fa3f2ec2d3eec0118d36
BGal23 <bartek.gal23@gmail.com>
2024-04-02 21:56:36 +0200

    page

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/shop">
+    <BrowserRouter basename="/shop_ts_react">
       <App />
     </BrowserRouter>
   </React.StrictMode>
