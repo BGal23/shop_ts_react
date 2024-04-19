31d67e39a4a324d288e83e274d6f2424bccfbee1
BGal23 <bartek.gal23@gmail.com>
2024-04-19 13:46:05 +0200

    Updates

--- assets/Home-DMO96G_Z.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-DI0SQG_6.js";const r=()=>e.jsx("p",{children:"Home"});export{r as default};
--- assets/NotFound-DNLP7Fhg.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-DI0SQG_6.js";const r=()=>o.jsx("p",{children:"NotFound"});export{r as default};
